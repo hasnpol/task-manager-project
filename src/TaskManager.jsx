bab718ed09822d20454fc91a97652ef296888992
Hasan Polat <hep1031@wildcats.unh.edu>
2024-04-27 18:05:24 -0400

    adding taskdata json and initially setting tasks

--- src/TaskManager.jsx
+++ src/TaskManager.jsx
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Container, ListGroup, ListGroupItem } from "reactstrap";
 import TaskForm from "./TaskForm";
 import DataPusher from "./DataPusher";
+import data from "../TaskData.json"; // Import userData.json
 import "./styles.css";
 
 // TaskManager component; contains a form and a list of tasks.
 // Contains TaskForm and DataPusher components.
 export default function TaskManager() {
   const [tasks, setTasks] = useState([]);
+
+  const setInitialTasks = () => {
+    localStorage.setItem("TaskData", JSON.stringify(data));
+    try {
+      // Retrieve JSON data from localStorage
+      const jsonData = localStorage.getItem("TaskData");
+
+      if (jsonData) {
+        // Parse JSON data
+        const taskData = JSON.parse(jsonData);
+        return taskData;
+      } else {
+        return null; // Return null if no data is found
+      }
+    } catch (error) {
+      console.error("Error retrieving data:", error.message);
+      return null;
+    }
+  };
 
   const handleTaskSubmit = (newTask) => {
     setTasks([...tasks, { ...newTask, id: tasks.length, completed: false }]);
@@ -24,6 +44,16 @@ export default function TaskManager() {
     const filteredTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(filteredTasks);
   };
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    const fetchData = async () => {
+      const data = setInitialTasks();
+      handleTaskSubmit(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Form className="Task">
@@ -60,3 +90,4 @@ export default function TaskManager() {
     </Form>
   );
 }
+
